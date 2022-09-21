fdb0fc6cbc3be4ed7ab6c9afe4872a32b04ea645
devnoman01 <devnoman01@gmail.com>
2022-09-21 22:42:37 +0600

    calculator using switch case

---
+++ 8 Switch case/calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cout << "Input two number: ";
+    cin >> n1 >> n2;
+
+    char op;
+    cout << "Input an operator: ";
+    cin >> op;
+
+    switch (op)
+    {
+    case '+':
+        cout << n1 + n2 << endl;
+        break;
+    case '-':
+        cout << n1 - n2 << endl;
+        break;
+    case '*':
+        cout << n1 * n2 << endl;
+        break;
+    case '/':
+        cout << n1 / n2 << endl;
+        break;
+    default:
+        cout << "Enter another operator";
+        break;
+    }
+
+    return 0;
+}
