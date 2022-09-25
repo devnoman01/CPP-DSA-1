0791337ebbd6b1b0a3dc615768843ae64d8d164a
devnoman01 <devnoman01@gmail.com>
2022-09-25 20:36:21 +0600

    sizeof() operator

---
+++ 9 Operators/sizeof.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    float b = 2;
+    double c = 3;
+    char d = 'A';
+
+    cout << sizeof(a) << endl;
+    cout << sizeof(b) << endl;
+    cout << sizeof(c) << endl;
+    cout << sizeof(d) << endl;
+
+    return 0;
+}
