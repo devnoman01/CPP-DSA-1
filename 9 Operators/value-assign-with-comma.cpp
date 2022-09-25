d811699aaee00c4a060491abd285b2bd1b8fda10
devnoman01 <devnoman01@gmail.com>
2022-09-25 20:54:44 +0600

    left to right multi value assign

---
+++ 9 Operators/value-assign-with-comma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5;
+
+    x = 1, 2, 3;
+
+    // left to right priority for assigning values
+
+    cout << x << endl;
+
+    return 0;
+}
