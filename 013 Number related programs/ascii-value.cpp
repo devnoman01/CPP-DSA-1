758650a1fa7c88c9fbde14c4c8fdde74a24be0b4
devnoman01 <devnoman01@gmail.com>
2022-09-29 19:53:31 +0600

    get ascii value of any character

---
+++ 013 Number related programs/ascii-value.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Enter character: ";
+    cin >> ch;
+
+    cout << "ASCII value: " << int(ch);
+
+    return 0;
+}
