1b0580670f8c8df4ec422077dd53a38946a8167a
devnoman01 <devnoman01@gmail.com>
2022-10-07 23:54:00 +0600

    running sum of array elements programs-2

---
+++ 018 Array/running-sum-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter array length: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        arr[i] += arr[i - 1];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
